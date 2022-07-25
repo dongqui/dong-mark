3d3a3c7c823c23cdf676f83a0ab8eccd0d47f6ab
Dongjin <dongqui9@gmail.com>
2022-07-26 01:51:34 +0900

    env: fork-ts-checker, alias path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config, options) {
+    const { dev, isServer } = options;
+
+    if (dev && isServer) {
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin(),
+      );
+    }
+
+    return config;
+  }
 }
 
 module.exports = nextConfig

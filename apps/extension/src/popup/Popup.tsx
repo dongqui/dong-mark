377c7708db153abbd9c94b884db166e8cf83bc1f
Dongjin <dongqui9@gmail.com>
2022-08-09 00:34:26 +0900

    env: set up tailwindcss

--- apps/extension/src/popup/Popup.tsx
+++ apps/extension/src/popup/Popup.tsx
@@ -1,5 +1,7 @@
+import './style.scss';
+
 function Popup() {
-  return <h1>hi</h1>;
+  return <h1 className="text-green-500">hi</h1>;
 }
 
 export default Popup;
--- apps/extension/webpack.config.js
+++ apps/extension/webpack.config.js
@@ -1,6 +1,8 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   mode: 'development',
@@ -15,13 +17,30 @@ module.exports = {
         test: /\.tsx$/,
         exclude: /node_modules/,
       },
+      {
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                ident: 'postcss',
+                plugins: [tailwindcss, autoprefixer],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+        test: /\.s[ac]ss$/i,
+      },
     ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve('src/manifest.json'),
+          from: path.resolve('src/static'),
           to: path.resolve('dist'),
         },
       ],

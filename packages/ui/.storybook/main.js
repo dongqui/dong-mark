33f6fcb1e931da1584980e3146f2d7a7faa1d46e
Dongjin <dongqui9@gmail.com>
2022-08-27 18:35:45 +0900

    env: ui 스토리북 추가

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,15 +1,41 @@
+const path = require('path');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
+  framework: "@storybook/react",
+  core: {
     "builder": "@storybook/builder-webpack5"
-  }
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.s[ac]ss$/i,      
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              ident: 'postcss',
+              plugins: [tailwindcss, autoprefixer],
+            },
+          },
+        },
+        'sass-loader',
+      ],        
+    });
+
+    config.resolve.modules.push(path.resolve(__dirname, '../src'));
+    return config;
+  },
 }

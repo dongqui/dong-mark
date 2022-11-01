79466545f45c4d90db395ab974cb5aef9d0919e9
dongqui <dongqui9@gmail.com>
2022-11-01 22:27:55 +0900

    feat: stytle constants

---
+++ packages/ui/src/constants/index.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  darkBG: '#222836',
+  darkBGElevated: '#3E4452',
+  contentBase: '#ffffff',
+  contentMedium: '#ABA6A6',
+  outline: '#2E2F32',
+};
+
+export const fontSizes = {
+  s: '14px',
+  m: '18px',
+  l: '26px',
+  xl: '30px',
+};
